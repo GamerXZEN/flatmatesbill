2f495cd776ee48164dc9efde4c7096ef2b45f179
FESL Sapphire Dev <codspecialops@gmail.com>
2023-02-25 18:05:43 -0600

    Initial Commit

--- backend.py
+++ backend.py
@@ -2,47 +2,40 @@ import time
 import webbrowser
 from fpdf import FPDF
 import os
-import filestack
 
 
 class PDFReport:
-	def __init__(self, **kwargs):
+	def __init__(self, person1, person2, amount1, amount2):
 		self.pdf = FPDF()
 		self.pdf.add_page()
-
-		self.pdf.image('misc_files/house.png', w=30, h=30)
-
+		try:
+			self.pdf.image('misc_files/house.png', w=30, h=30)
+		except:
+			print("Please reload")
 		self.pdf.set_font('Arial', 'B', 24)
 		self.pdf.cell(40, 10, txt=f"Flatmates Bill", ln=3)
-		for value in kwargs.values():
-			for index, lv in enumerate(value):
-				self.pdf.set_font('Helvetica', 'B', 12)
-				self.pdf.cell(40, 10, f"Person {int(index) + 1} Amount: ", border=True)
-				self.pdf.cell(40, 10, txt=str(round(float(value[index]), 2)), border=True, ln=1)
+		self.pdf.set_font('Helvetica', 'B', 12)
+		self.pdf.cell(70, 10, f"{person1.title()}'s Amount: ", border=True)
+		self.pdf.cell(40, 10, txt=str(round(float(amount1), 2)), border=True, ln=1)
+		self.pdf.cell(70, 10, f"{person2.title()}'s Amount: ", border=True)
+		self.pdf.cell(40, 10, txt=str(round(float(amount2), 2)), border=True, ln=1)
 
 		self.pdf.cell(40, 10, txt=f"Billed at {time.strftime('%H:%M:%S - %d/%m%Y')}.")
-		client = filestack.Client(os.getenv('FILESTACK_ACCESS_KEY'))
 
-		new_file = client.upload(filepath="misc_files/report.pdf")
-		print(new_file.url)
 		os.chdir('misc_files')
 		self.pdf.output("report.pdf")
 		webbrowser.open('report.pdf')
+
+	def get_url(self):
+		return self.new_file.url
 
 
 class Bill:
 	def __init__(self, amount):
 		self.amount = amount
 
-	def calc_amount(self, **kwargs):
-		person_list = []
-		for value in kwargs.values():
-			if value:
-				if type(value) == list:
-					for index, lv in enumerate(value):
-						int_value = float(value[index] / sum(value) * self.amount)
-						person_list.append(str(int_value))
-				else:
-					int_value = value / 30 * self.amount
-					person_list.append(int_value)
-		return person_list
+	def calc_amount(self, person1, person2):
+		total = person1 + person2
+		person1 = person1 / total * self.amount
+		person2 = person2 / total * self.amount
+		return person1, person2
--- main.py
+++ main.py
@@ -1,14 +1,53 @@
-from backend import PDFReport, Bill
+from wtforms.validators import DataRequired
+from backend import *
+from flask.views import MethodView
+from flask import Flask, render_template, request
+from wtforms import Form, StringField, SubmitField
+
+
+app = Flask(__name__)
+
+
+class HomePage(MethodView):
+
+	def get(self):
+		return render_template('index.html')
+
+
+class BillFormPage(MethodView):
+
+	def get(self):
+		bill_form = BillForm()
+		return render_template('bill.html', bill_form=bill_form)
+
+
+class ResultsPage(MethodView):
+
+	def post(self):
+		bill_form = BillForm(request.form)
+		amount = int(bill_form.amount.data)
+		person_1_days = int(bill_form.days_in_house1.data)
+		person_2_days = int(bill_form.days_in_house2.data)
+		person1_name = bill_form.flatmate1.data
+		person2_name = bill_form.flatmate2.data
+		bill = Bill(amount)
+		person1_amount, person2_amount = bill.calc_amount(person_1_days, person_2_days)
+		PDFReport(person1_name, person2_name, person1_amount, person2_amount)
+		result = f"{person1_name.title()} pays {person1_amount} <br> {person2_name.title()} pays {person2_amount}"
+		return result
+
+
+class BillForm(Form):
+	amount = StringField('Bill Amount: ', validators=[DataRequired()])
+	flatmate1 = StringField(f'Flatmate No 1: ', validators=[DataRequired()])
+	days_in_house1 = StringField(f'Days in House: ', validators=[DataRequired()])
+	flatmate2 = StringField(f'Flatmate No 2: ', validators=[DataRequired()])
+	days_in_house2 = StringField(f'Days in House: ', validators=[DataRequired()])
+	submit_button = SubmitField('Submit')
+
 
 if __name__ == '__main__':
-	total_amount = int(input("What was your total amount: "))
-	bill = Bill(total_amount)
-	flatmates_no = int(input("How many people live in the flat: "))
-	flatmates_list = []
-	for index in range(0, flatmates_no):
-		flatmate = int(input(f"How many days did each person stay: "))
-		flatmates_list.append(flatmate)
-	calc_list = bill.calc_amount(flatmates_list=flatmates_list)
-	for index, value in enumerate(calc_list):
-		print(f"Person {index + 1} Amount: {value}")
-	pdf = PDFReport(flatmates_list=calc_list)
+	app.add_url_rule('/', view_func=HomePage.as_view('home'))
+	app.add_url_rule('/bill/', view_func=BillFormPage.as_view('bill'))
+	app.add_url_rule('/result/', view_func=ResultsPage.as_view('results'))
+	app.run(debug=True)
